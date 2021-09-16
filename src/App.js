21fc1c9608bc9d1fa4bbdef5f075b7d1742cc65a
CarolinaMisa <1700250@ucc.edu.ar>
2021-09-16 19:58:17 -0300

    Chokidar bug
    [https://github.com/CarolinaMisa/SFE-memoryGame/issues/2]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
 import Dashboard from './components/Dashboard';
 
 
-const App = () => {
+function App() {
     return (
-        <div>
+        <Router>
             <Dashboard/>
-        </div>
+            <Switch>
+                <Route path="/" exact></Route>
+                <Route path="/numbers">
+                </Route>
+                <Route path="/words">
+                </Route>
+                <Route path="/colors">
+                </Route>
+                <Route path="/additions">
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
