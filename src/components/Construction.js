7f601b8de872409e087e423f6aa98c4ea23e9f25
Cindy Ortega <cindyortega416@gmail.com>
2021-10-05 00:01:24 -0300

    Creación de MyStyle.js. Cambios en Dashboard.js. Cambios en Construction.js de acuerdo a stackoverflow

--- src/components/Construction.js
+++ src/components/Construction.js
@@ -1,69 +1,80 @@
-import {Component} from "react";
+import {useStyles} from "./MyStyle";
 import {getInitialState} from "./Dashboard";
+import {Component} from "react";
 
-class Construction extends Component {
-    constructor(props) {
-        super(props);
-        this.state = getInitialState();
-    }
+class Construction extends Component{
+    state = {
+        searchNodes: " "
+    };
 
-    selectCard(card) {
-        if (
-            this.state.isComparing ||
-            this.state.selectedCouple.indexOf(card) > -1 ||
-            card.wasGuessed
-        ) {
-            return;
-        }
+    render() {
+        const {classes} = this.props;
+        return(
+            selectCard(card)
+            {
+                if (
+                    this.state.isComparing ||
+                    this.state.selectedCouple.indexOf(card) > -1 ||
+                    card.wasGuessed
+                ) {
+                    return;
+                }
 
-        const selectedCouple = [...this.state.selectedCouple, card];
-        this.setState({
-            selectedCouple
-        });
-
-        if (selectedCouple.length === 2) {
-            this.compareCouple(selectedCouple);
-        }
-    }
-    compareCouple(selectedCouple){
-        this.setState({isComparing: true});
-
-        setTimeout(() => {
-            const [firstCard, secondCard] = selectedCouple;
-            let deck = this.state.deck;
-
-            if (firstCard.icon === secondCard.icon){
-                deck = deck.map((card) => {
-                    if (card.icon !== firstCard.icon) {
-                        return card;
-                    }
-                    return {...card, wasGuessed: true}
+                const selectedCouple = [...this.state.selectedCouple, card];
+                this.setState({
+                    selectedCouple
                 });
+
+                if (selectedCouple.length === 2) {
+                    this.compareCouple(selectedCouple);
+                }
             }
-            this.checkIfWinner(deck);
-            this.setState({
-                selectedCouple: [],
-                deck,
-                isComparing: false,
-                numberOfAttempts: this.state.numberOfAttemps + 1
-            })
-        }, 1000)
-    }
-    checkIfWinner(deck) {
-        if(
-            deck.filter((card) => !card.wasGuessed).length === 0
-        ) {
-            alert(`Ganaste en ${this.state.numberOfAttemps} intentos!`);
+
+           compareCouple(selectedCouple)
+            {
+                this.setState({isComparing: true});
+
+                setTimeout(() => {
+                    const [firstCard, secondCard] = selectedCouple;
+                    let deck = this.state.deck;
+
+                    if (firstCard.icon === secondCard.icon) {
+                        deck = deck.map((card) => {
+                            if (card.icon !== firstCard.icon) {
+                                return card;
+                            }
+                            return {...card, wasGuessed: true}
+                        });
+                    }
+                    this.checkIfWinner(deck);
+                    this.setState({
+                        selectedCouple: [],
+                        deck,
+                        isComparing: false,
+                        numberOfAttempts: this.state.numberOfAttemps + 1
+                    })
+                }, 1000)
+            }
+           checkIfWinner(deck)
+            {
+                if (
+                    deck.filter((card) => !card.wasGuessed).length === 0
+                ) {
+                    alert(`Ganaste en ${this.state.numberOfAttemps} intentos!`);
+                }
+            }
+
+          resetGame()
+            {
+                this.setState(
+                    getInitialState()
+                );
+
+            }
         }
-    }
 
-    resetGame() {
-        this.setState(
-            getInitialState()
-        );
 
-    }
-
+)
 
 }
-export default Construction;
+export default useStyles(useStyles)(Construction)
